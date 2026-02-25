846b64f886c1e5d698b76c1d76dced6a868516eb
Arun Shivhare <arunshivhare925@gmail.com>
2026-02-25 23:38:01 +0530

    update identity

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='fixed bottom-0 w-full flex justify-center bg-white border-t border-yellow-100 shadow-inner py-3'>
-      <p>Created by Arun &copy;2026 All rights reserved</p>
+      <p>Developed By Arun | <span className="text-yellow-900"> &lt;</span>
+          Pass
+          <span className="text-yellow-900">Man/&gt;</span> &copy; 2026 All rights reserved.</p>
     </div>
   )
 }
