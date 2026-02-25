a55a7d94ef5afe105cf8638b5986097186a198f3
Arun Shivhare <arunshivhare925@gmail.com>
2026-02-25 12:43:04 +0530

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import "./App.css";
-import Navbar from "./components/navbar";
 import Manager from "./components/Manager";
 import Footer from "./components/Footer";
 import Login from "./components/Login";
+import Navbar from "./components/Navbar";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
