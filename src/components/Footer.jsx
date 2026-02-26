6e03f345124a7ac63079efe2f0ea95f5871402e5
Arun Shivhare <arunshivhare925@gmail.com>
2026-02-26 18:52:30 +0530

    text-center

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='fixed bottom-0 w-full flex justify-center bg-white border-t border-yellow-100 shadow-inner py-3'>
+    <div className='fixed bottom-0 w-full flex text-center justify-center bg-white border-t border-yellow-100 shadow-inner py-3'>
       <p>Developed By Arun | <span className="text-yellow-900"> &lt;</span>
           Pass
           <span className="text-yellow-900">Man/&gt;</span> &copy; 2026 All rights reserved.</p>
